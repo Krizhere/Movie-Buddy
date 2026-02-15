180392c0cbb4e0398ad9868ab5e2e391926d9eb5
Krish Kumar <imkriz001@gmail.com>
2026-02-15 23:50:11 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import requests
+
+@st.cache_data(show_spinner=False)
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        response = requests.get(url, headers=headers, timeout=5)
+
+        if response.status_code != 200:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+        data = response.json()
+        poster_path = data.get("poster_path")
+
+        if not poster_path:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+
+    except requests.exceptions.RequestException:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+movies_list=pickle.load(open('movie.pkl','rb'))
+movies=pd.DataFrame(movies_list)
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+st.title("Movie Recommender System")
+selected_movie=st.selectbox("Select or Type Movie Name",movies['title'].values)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
